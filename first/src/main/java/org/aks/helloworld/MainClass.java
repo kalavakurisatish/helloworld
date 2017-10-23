c87f64ed259ddbed5e966660f5bbff1deff55c8c
AR01138 <AR01138@LP-76396.noa.nl.rsg>
2017-10-23 11:31:02 +0200

    initial push

--- first/src/main/java/org/aks/helloworld/MainClass.java
+++ first/src/main/java/org/aks/helloworld/MainClass.java
@@ -4,7 +4,8 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world !");
+//		System.out.println("Hello world !");
+		PrintOutput.printToConsole("Hello world !");
 	}
 
 }
